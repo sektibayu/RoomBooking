94e5e82d3ee70cb3f297deffa0636046bf8bc93f
FARHAN\LENOVO <mumulmln1997@gmail.com>
2016-12-15 20:47:08 +0700

    model (belum revisi)

--- app/DBReservation.php
+++ app/DBReservation.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class DBReservation extends Model
 {
-    
+ 	protected $table='reservation';
+ 	protected $primaryKey='reservation_id';
+    public $timestamps=false;
+    protected $fillable=[
+    'nrp_nip',
+    'room_id',
+    'start_time',
+    'end_time',
+    'status'
+    ];
+
+ 	public function room()
+ 	{
+ 		return $this->belongsTo('App/DBRoom');
+ 	}
+
+ 	public function user()
+ 	{
+ 		return $this->belongsTo('App/DBUser');
+ 	}  
 }
--- app/DBUser.php
+++ app/DBUser.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class DBUser extends Model
 {
-    //
+    protected $table='user';
+ 	protected $primaryKey='nrp_nip';
+    public $timestamps=false;
+    protected $fillable=[
+    'name'
+    ];
+
+ 	public function reservation()
+ 	{
+ 		return $this->hasMany('App/DBReservation');
+ 	}
 }
